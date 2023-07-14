a10f99cbd80545af33824c920f90b712e553d021
CarlosACardoso <carloalbertocardoso@gmail.com>
2023-07-14 19:26:24 -0300

    Criei um modelo de registro de dados e agora é utilizado dessa forma.

---
+++ NodeJs/src/data/ListaSimples.js
@@ -0,0 +1,30 @@
+module.exports = [
+    {
+      id: 1,
+      image: '',
+      name: 'Espelho Goldran n5',
+      brand: 'Dental Especial',
+      quantity: 1,
+      optional: 'Necessário',
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      image: '',
+      name: 'Resina Z100 - 3M',
+      brand: 'Dental Alto Astral',
+      quantity: 4,
+      optional: 'Necessário',
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      image: '',
+      name: 'Luva Cirúrgica',
+      brand: 'Dental Fundamental',
+      quantity: 1,
+      optional: 'Opcional',
+      isCompleted: true,
+    }
+];
+  
--- NodeJs/src/index.js
+++ NodeJs/src/index.js
@@ -1,43 +1,40 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
-const app = express()
-const router = express.Router()
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const router = express.Router();
 
-const items = [
-    {
-      id: 1,
-      image: '',
-      name: "Espelho Goldran n5",
-      brand: "Dental Especial",
-      quantity: 1,
-      optional: "Necessário",
-      isCompleted: false,
-    },
-    {
-      id: 2,
-      image: '',
-      name: "Resina Z100 - 3M",
-      brand: "Dental Alto Astral",
-      quantity: 4,
-      optional: "Necessário",
-      isCompleted: false,
-    },
-    {
-      id: 3,
-      image: '',
-      name: "Luva Cirúrgica mega legalaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaa aaaaaaaaaaaaa",
-      brand: "Dental Fundamental",
-      quantity: 1,
-      optional: "Opcional",
-      isCompleted: true,
-    }
-]
+let items = [];
+const filesPath = path.join(__dirname, 'data');
 
-router.get('/suppliesList' , (request, response) => {
-    response.send(items)
-})
+const saveItems = (items, filesPath) => {
+    const fileName = 'ListaSimples.js';
+    const fileContent = `module.exports = ${JSON.stringify(items, null, 2)};`;
+    const fullPath = path.join(filesPath, fileName);
+  
+    fs.writeFile(fullPath, fileContent, (err) => {
+      if (err) {
+        console.error(`Erro ao salvar arquivo ${fullPath}: ${err}`);
+      } else {
+        console.log(`Arquivo ${fullPath} salvo com sucesso.`);
+      }
+    });
+  };
+  
+
+const readItems = (fileName) => {
+  const fullPath = path.join(filesPath, fileName);
+  const fileContent = require(fullPath);
+  items = fileContent;
+};
+
+router.get('/suppliesList', (request, response) => {
+  readItems('ListaSimples.js');
+  response.send(items);
+});
 
 app.use(cors());
-app.use(router)
+app.use(router);
 
-app.listen(3000, () => console.log('Server started at port 3000'))
+app.listen(3000, () => console.log('Server started at port 3000'));
